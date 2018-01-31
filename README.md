69bce909fd729c81d0190c7b5fb06d618ba0222c
asinha94 <asinha94@gmail.com>
2018-01-31 01:09:03 -0800

    added config file loader. updated readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# utilities
+# Utilities
 Utilities that some might find useful for the project
+
+# Config file 
+
+Given a JSON file called `config.json`
+
+```
+{
+    "serverConfigs" : {
+        "webserver" : {
+            "address": "0.0.0.1",
+            "port": "44440",
+            "protocol": "http"
+        },
+
+        "transaction" : {
+            "address": "localhost",
+            "port": "44441",
+            "protocol": "tcp"
+        }
+    },
+
+    "misc" : {
+        "htmlLoc" : "../frontend/"
+    } 
+}
+```
+
+It can be imported like this
+
+```
+import (
+    "github.com/RATDistributedSystems/utilities"
+)
+
+func main() {
+    config := utilities.LoadConfigs("config.json")
+
+    //to get transaction server details
+    addr, protocol := config.GetServerDetails("transaction")
+
+    // get something from the misc map
+    loc := config.GetValue("htmlLoc")
+}
+```
---
+++ configureserver.go
@@ -0,0 +1,73 @@
+package utilities
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type jsonConfiguration struct {
+	address  string
+	port     string
+	protocol string
+}
+
+type Configuration struct {
+	serverConfigs map[string]jsonConfiguration
+	misc          map[string]string
+}
+
+var (
+	PackageConfiguration = Configuration{
+		serverConfigs: make(map[string]jsonConfiguration),
+		misc:          make(map[string]string)}
+)
+
+func LoadConfigs(filename string) *Configuration {
+	f, errIO := ioutil.ReadFile(filename)
+	if errIO != nil {
+		log.Panicf("Can't read configuration file %s. Panicing.", filename)
+	}
+
+	var parsedJSON map[string]interface{}
+	err := json.Unmarshal(f, &parsedJSON)
+	if err != nil {
+		log.Panicf("Can't parse json: %s. Panicing", f)
+	}
+
+	configs := parsedJSON["serverConfigs"].(map[string]interface{})
+	for k, v := range configs {
+		vals := v.(map[string]interface{})
+		jsonTemp := jsonConfiguration{}
+		for i, j := range vals {
+			switch i {
+			case "address":
+				jsonTemp.address = j.(string)
+			case "port":
+				jsonTemp.port = j.(string)
+			case "protocol":
+				jsonTemp.protocol = j.(string)
+			}
+		}
+		PackageConfiguration.serverConfigs[k] = jsonTemp
+	}
+
+	misc := parsedJSON["misc"].(map[string]interface{})
+	for k, v := range misc {
+		PackageConfiguration.misc[k] = v.(string)
+	}
+	return &PackageConfiguration
+}
+
+func (c *Configuration) GetServerDetails(name string) (addr string, protocol string) {
+	ip := c.serverConfigs[name].address
+	port := c.serverConfigs[name].port
+	addr = fmt.Sprintf("%s:%s", ip, port)
+	protocol = c.serverConfigs[name].protocol
+	return
+}
+
+func (c *Configuration) GetValue(key string) string {
+	return c.misc[key]
+}
