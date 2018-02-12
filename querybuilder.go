96056e2b6741188df9e9bb8811e07d88c62aa78b
asinha94 <anuraagsinha@hotmail.com>
2018-02-12 15:55:08 -0800

    added query file. not complete

---
+++ querybuilder.go
@@ -0,0 +1,12 @@
+package utilities
+
+const (
+	selectStatement = "SELECT %S FROM %s WHERE %s"
+	insertStatement = "INSERT INTO %s (%s) VALUES (%s)"
+	updateStatement = ""
+	deleteStatement = "DELETE FROM %s where %s"
+)
+
+func tester() {
+
+}
