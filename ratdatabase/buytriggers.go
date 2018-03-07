c055fafd647eec631649ab02d4723491ab7fbc31
asinha94 <asinha94@gmail.com>
2018-03-07 01:50:13 -0800

    added cql trigger functions

--- ratdatabase/buytriggers.go
+++ ratdatabase/buytriggers.go
@@ -1,1 +1,53 @@
 package ratdatabase
+
+import (
+	"github.com/gocql/gocql"
+)
+
+func BuyTriggerExists(userID string, stockName string) (exists bool, amount int) {
+	qry := createSelectQuery(stringArray(pendingcash), buytrigger, stringArray(userid, stock))
+	rs, count := executeSelectCassandraQuery(qry, userID, stockName)
+
+	if count < 1 {
+		return
+	}
+
+	exists = true
+	amount = castInt(rs[0][pendingcash])
+	return
+}
+
+func InsertSetBuyTrigger(userID string, stockName string, cashAmount int) (oldAmount int) {
+	exists, oldAmount := BuyTriggerExists(userID, stockName)
+
+	if exists {
+		qry := createUpdateStatement(buytrigger, stringArray(pendingcash), stringArray(userid, stock))
+		executeCassandraQuery(qry, cashAmount, userID, stockName)
+	} else {
+		qry := createInsertStatement(buytrigger, stringArray(pendingTTID, pendingcash, stock, userid))
+		uuid, _ := gocql.RandomUUID()
+		executeCassandraQuery(qry, uuid, cashAmount, stockName, userID)
+	}
+	return
+}
+
+func UpdateBuyTriggerPrice(userID string, stockName string, triggerPrice int) (triggerAmountSet bool) {
+	triggerAmountSet, _ = BuyTriggerExists(userID, stockName)
+
+	if triggerAmountSet {
+		qry := createUpdateStatement(buytrigger, stringArray(triggervalue), stringArray(userid, stock))
+		executeCassandraQuery(qry, triggerPrice, userID, stockName)
+	}
+
+	return
+}
+
+func CancelBuyTrigger(userID string, stockName string) (buyAmount int) {
+	_, buyAmount = BuyTriggerExists(userID, stockName)
+	if buyAmount == 0 {
+		return
+	}
+	qry := createDeleteStatement(buytrigger, stringArray(userid, stock))
+	executeCassandraQuery(qry, userID, stockName)
+	return
+}
--- ratdatabase/selltriggers.go
+++ ratdatabase/selltriggers.go
@@ -1,1 +1,51 @@
 package ratdatabase
+
+import "github.com/gocql/gocql"
+
+func SellTriggerExists(userID string, stockName string) (exists bool, amount int) {
+	qry := createSelectQuery(stringArray(stockamount), selltrigger, stringArray(userid, stock))
+	rs, count := executeSelectCassandraQuery(qry, userID, stockName)
+
+	if count < 1 {
+		return
+	}
+
+	exists = true
+	amount = castInt(rs[0][stockamount])
+	return
+}
+
+func InsertSetSellTrigger(userID string, stockName string, stockAmount int) (oldStockSellAmount int) {
+	exists, oldStockSellAmount := SellTriggerExists(userID, stockName)
+
+	if exists {
+		qry := createUpdateStatement(selltrigger, stringArray(stockamount), stringArray(userid, stock))
+		executeCassandraQuery(qry, stockAmount, userID, stockName)
+	} else {
+		qry := createInsertStatement(selltrigger, stringArray(pendingTTID, stockamount, stock, userid))
+		uuid, _ := gocql.RandomUUID()
+		executeCassandraQuery(qry, uuid, stockAmount, stockName, userID)
+	}
+	return
+}
+
+func UpdateSellTriggerPrice(userID string, stockName string, triggerValue int) (triggerAmountSet bool) {
+	triggerAmountSet, _ = SellTriggerExists(userID, stockName)
+
+	if triggerAmountSet {
+		qry := createUpdateStatement(selltrigger, stringArray(triggervalue), stringArray(userid, stock))
+		executeCassandraQuery(qry, triggerValue, userID, stockName)
+	}
+
+	return
+}
+
+func CancelSellTrigger(userID string, stockName string) (oldSellAmonut int) {
+	_, oldSellAmonut = SellTriggerExists(userID, stockName)
+	if oldSellAmonut == 0 {
+		return
+	}
+	qry := createDeleteStatement(selltrigger, stringArray(userid, stock))
+	executeCassandraQuery(qry, userID, stockName)
+	return
+}
